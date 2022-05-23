9efb6fd057046d70c3a4b5179b088ac30aefd26f
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-05-23 10:45:00 -0300

    Create exe003.py

---
+++ modulo 01/exercicio003/exe003.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+sub = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+r = n1 % n2
+e = n1 ** n2
+print(f'Soma: {s} \nSubtração: {sub} \nMultiplicação: {m} \nDivisão: {d} \nDivisão Inteira: {di} \nResto: {r} \nExponenciação: {e}')
