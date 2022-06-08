263f6636f5006f92e56ec00ec7b80c28873be8c7
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-06-08 10:07:27 -0300

    Create exe010.py

---
+++ modulo 01/exercicio010/exe010.py
@@ -0,0 +1,5 @@
+largura = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
+area = largura * altura
+litros = area / 2
+print(f'Para pintar uma parede de largura {largura} metros e altura {altura} metros com total de área de {area} m², serão necessários {litros} litros de tinta.')
