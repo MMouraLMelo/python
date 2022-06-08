33259cdc86f721739df066c15f88ff161269060d
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-06-08 08:48:56 -0300

    atualizando

---
+++ modulo 01/exercicio004/exe004.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+ant = num - 1
+suc = num + 1
+print(f'O antecessor do {num} é: {ant}')
+print(f'O sucessor do {num} é: {suc}')
---
+++ modulo 01/exercicio005/exe005.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número: '))
+d = num * 2
+t = num * 3
+rq = num ** (1/2)
+print(f'O dobro de {num} é: {d}')
+print(f'O triplo de {num} é: {t}')
+print(f'A raiz quadrada de {num} é: {rq}')
