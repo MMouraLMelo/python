db2ce755a3090f2ede1f307f7e465318a15535a7
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-06-08 09:53:11 -0300

    Create exe009.py

---
+++ modulo 01/exercicio009/exe009.py
@@ -0,0 +1,5 @@
+din = float(input('Quanto dinheiro você quer converter? R$ '))
+dol = din / 4.90
+euro = din / 5.26
+print(f'Com R$ {din}, você pode comprar US$ {dol}.')
+print(f'Com R$ {din}, você pode comprar € {euro}')
