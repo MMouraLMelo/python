44703cf9d1d404eaf4fab8be63757b8295fef44c
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-06-09 09:55:50 -0300

    Create exe018.py

---
+++ modulo 01/exercicio018/exe018.py
@@ -0,0 +1,8 @@
+from random import choice
+nome1 = input('Digite o primeiro nome: ')
+nome2 = input('Digite o segundo nome: ')
+nome3 = input('Digite o terceiro nome: ')
+nome4 = input('Digite o quarto nome: ')
+lista = [nome1, nome2, nome3, nome4]
+esc = choice(lista)
+print(f'O nome escolhido foi: {esc}')
