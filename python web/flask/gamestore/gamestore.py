fb3856de924d9e05eff3bf95e6b18239f160805e
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-10-14 16:52:21 -0300

    atualizando

---
+++ python web/flask/gamestore/gamestore.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, session, flash
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+app.secret_key = 'Parangaricotirimirruaro'
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado!')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')
+
+app.run(debug=True)
+
