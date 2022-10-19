8179a7cf1b4de89f822a77ff64a960425f67c99a
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-10-19 09:21:26 -0300

    atualizando

--- python web/flask/gamestore/config.py
+++ python web/flask/gamestore/config.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD='mysql+mysqlconnector',
         usuario='root',
-        senha='Marcel1987',
+        senha='',
         servidor='localhost',
         database='jogoteca'
     )
--- python web/flask/gamestore/prepara_banco.py
+++ python web/flask/gamestore/prepara_banco.py
@@ -7,7 +7,7 @@ try:
       conn = mysql.connector.connect(
             host='127.0.0.1',
             user='root',
-            password='Marcel1987'
+            password=''
       )
 except mysql.connector.Error as err:
       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
